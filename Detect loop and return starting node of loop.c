7c537fad33d3161683844b8ea621e6ebe0c8c8a2
NikithaLingineni <113195326+NikithaLingineni@users.noreply.github.com>
2022-10-28 12:04:37 +0530

    Add files via upload

---
+++ Detect loop and return starting node of loop.c
@@ -0,0 +1,67 @@
+#include<stdio.h>
+#include<stdlib.h>
+struct node
+{
+	int data;
+	struct node*next;
+};
+struct node*head,*temp,*fast,*slow;
+struct node* detectloop(struct node *head) 
+{
+    slow=head;
+    fast=head;
+    while(fast!=NULL &&fast->next!=NULL)
+    {
+    	slow=slow->next;
+        fast=fast->next->next;
+        if(fast==slow)
+        break;
+    }
+    slow=head;
+    while(slow!=fast)
+    {
+    	slow=slow->next;
+        fast=fast->next;
+	}
+    return slow; 
+}
+void display()
+{
+	temp=head;
+	printf("\nLinked list is :\n");
+	while(temp!=NULL)
+	{
+		printf("%d ",temp->data);
+		temp=temp->next;
+	}
+}
+int main()
+{
+	struct node*newnode;
+	int choice=1;
+	while(choice)
+	{
+		newnode=(struct node*)malloc(sizeof(struct node));
+		newnode->next=NULL;
+		printf("Enter the data you want to be inserted:");
+		scanf("%d",&newnode->data);
+		if(head==NULL)
+		{
+			head=temp=newnode;
+		}
+		else
+		{
+			temp->next=newnode;
+			temp=newnode;
+		}
+		printf("Do you want to continue (1 or 0)?");
+		scanf("%d",&choice);
+	}
+	head->next->next->next->next->next=head->next->next;
+	struct node*res=detectloop(head);
+	if(res==NULL)
+	   printf("\nloop not found");
+	else
+	printf("\n loop found and starting node is %d ",res->data);
+	return 0;
+}
